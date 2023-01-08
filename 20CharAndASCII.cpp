1b700e2c21b9fd126f4bd76f0f7ac1aa64102336
3xx <fhawaa@gmail.com>
2023-01-08 15:33:52 -0800

    Add files via upload

---
+++ 20CharAndASCII.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main()
+{
+   
+
+
+	cout << " Enter Your Ascii-Value : "; // ادخل الأسكي فاليو 
+	int x;
+	cin >> x;
+	cout << "[>] " << char(x) << endl;
+	cout << " Enter Your charctor : "; // أدخل الحرف
+	char y;
+	cin >> y;
+	cout << "[>] " << int(y) << endl;
+
+	return 0;
+}
+
